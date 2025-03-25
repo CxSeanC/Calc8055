d041f4fbcb5b16a4478b38989d8fd475f932accd
CxSeanC <Sean.Carroll@checkmarx.com>
2025-03-25 12:35:30 -0400

    Changes for build

---
+++ build/CMakeFiles/vulncalc.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/calculator.c" "CMakeFiles/vulncalc.dir/src/calculator.c.obj" "gcc" "CMakeFiles/vulncalc.dir/src/calculator.c.obj.d"
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/api_server.cpp" "CMakeFiles/vulncalc.dir/src/api_server.cpp.obj" "gcc" "CMakeFiles/vulncalc.dir/src/api_server.cpp.obj.d"
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/archive_utils.cpp" "CMakeFiles/vulncalc.dir/src/archive_utils.cpp.obj" "gcc" "CMakeFiles/vulncalc.dir/src/archive_utils.cpp.obj.d"
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/json_parser.cpp" "CMakeFiles/vulncalc.dir/src/json_parser.cpp.obj" "gcc" "CMakeFiles/vulncalc.dir/src/json_parser.cpp.obj.d"
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/logger.cpp" "CMakeFiles/vulncalc.dir/src/logger.cpp.obj" "gcc" "CMakeFiles/vulncalc.dir/src/logger.cpp.obj.d"
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/main.cpp" "CMakeFiles/vulncalc.dir/src/main.cpp.obj" "gcc" "CMakeFiles/vulncalc.dir/src/main.cpp.obj.d"
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/network.cpp" "CMakeFiles/vulncalc.dir/src/network.cpp.obj" "gcc" "CMakeFiles/vulncalc.dir/src/network.cpp.obj.d"
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/secrets.cpp" "CMakeFiles/vulncalc.dir/src/secrets.cpp.obj" "gcc" "CMakeFiles/vulncalc.dir/src/secrets.cpp.obj.d"
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/ui.cpp" "CMakeFiles/vulncalc.dir/src/ui.cpp.obj" "gcc" "CMakeFiles/vulncalc.dir/src/ui.cpp.obj.d"
+  "C:/Users/SeanCa/Downloads/CalcSec8055/src/utils.cpp" "CMakeFiles/vulncalc.dir/src/utils.cpp.obj" "gcc" "CMakeFiles/vulncalc.dir/src/utils.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/vulncalc.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulncalc.dir/src/api_server.cpp.obj"
+  "CMakeFiles/vulncalc.dir/src/api_server.cpp.obj.d"
+  "CMakeFiles/vulncalc.dir/src/archive_utils.cpp.obj"
+  "CMakeFiles/vulncalc.dir/src/archive_utils.cpp.obj.d"
+  "CMakeFiles/vulncalc.dir/src/calculator.c.obj"
+  "CMakeFiles/vulncalc.dir/src/calculator.c.obj.d"
+  "CMakeFiles/vulncalc.dir/src/json_parser.cpp.obj"
+  "CMakeFiles/vulncalc.dir/src/json_parser.cpp.obj.d"
+  "CMakeFiles/vulncalc.dir/src/logger.cpp.obj"
+  "CMakeFiles/vulncalc.dir/src/logger.cpp.obj.d"
+  "CMakeFiles/vulncalc.dir/src/main.cpp.obj"
+  "CMakeFiles/vulncalc.dir/src/main.cpp.obj.d"
+  "CMakeFiles/vulncalc.dir/src/network.cpp.obj"
+  "CMakeFiles/vulncalc.dir/src/network.cpp.obj.d"
+  "CMakeFiles/vulncalc.dir/src/secrets.cpp.obj"
+  "CMakeFiles/vulncalc.dir/src/secrets.cpp.obj.d"
+  "CMakeFiles/vulncalc.dir/src/ui.cpp.obj"
+  "CMakeFiles/vulncalc.dir/src/ui.cpp.obj.d"
+  "CMakeFiles/vulncalc.dir/src/utils.cpp.obj"
+  "CMakeFiles/vulncalc.dir/src/utils.cpp.obj.d"
+  "libvulncalc.dll.a"
+  "vulncalc.exe"
+  "vulncalc.exe.manifest"
+  "vulncalc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/vulncalc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
